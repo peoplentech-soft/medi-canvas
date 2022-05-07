203aa826e476ca0073b4e063e165c185c8e742fb
Nazmus Sakib <nazmus@piit.local>
2022-05-07 19:00:59 +0600

    popup modal added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import banner from '../../img/header/header-banner.jpg';
+import Appointment from '../Appointment/Appointment';
 
 const Header = () => {
     return (
@@ -10,10 +11,31 @@ const Header = () => {
                     <p>Total Health <span className="color-brand">Care</span> Solution</p>
                     <h1>Your Most Trusted <span className="color-brand">Health</span> Partner</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam adipisci error quod. Amet facere enim a, natus fugit consequatur? Incidunt reprehenderit porro corrupti necessitatibus rem nam pariatur amet nemo?</p>
-                    <button className="btn button-brand">Make Appointment</button>
+                    <button type="button" className="btn button-brand" data-bs-toggle="modal" data-bs-target="#exampleModal">Make Appointment</button>
                 </div>
                 <div className="header-banner col-lg-6 col-md-12">
-                    <img src={banner} alt="" className="img-fluid"/>
+                    <img src={banner} alt="" className="img-fluid" />
+                </div>
+            </div>
+
+
+
+            
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">MediCanvas</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <Appointment/>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn button-brand" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
