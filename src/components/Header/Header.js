524b339b18b3ac2fd2aeb25e29efcc526d0dfd70
Nazmus Sakib <nazmus@piit.local>
2022-05-07 17:00:17 +0600

    update navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,14 @@ import banner from '../../img/header/header-banner.jpg';
 const Header = () => {
     return (
         <div className="header">
-            <div className="header-container container-brand d-flex justify-content-between align-items-center">
-                <div className="header-content col-md-6">
+            <div className="row header-container container-brand d-flex justify-content-between align-items-center">
+                <div className="header-content col-lg-6 col-md-12">
                     <p>Total Health <span className="color-brand">Care</span> Solution</p>
                     <h1>Your Most Trusted <span className="color-brand">Health</span> Partner</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam adipisci error quod. Amet facere enim a, natus fugit consequatur? Incidunt reprehenderit porro corrupti necessitatibus rem nam pariatur amet nemo?</p>
                     <button className="btn button-brand">Make Appointment</button>
                 </div>
-                <div className="header-banner col-md-6">
+                <div className="header-banner col-lg-6 col-md-12">
                     <img src={banner} alt="" className="img-fluid"/>
                 </div>
             </div>
